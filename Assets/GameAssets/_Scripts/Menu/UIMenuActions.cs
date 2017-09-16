b493ea89398f241bdabf48696a31fba00c9a15bf
Julio Aguilera <aguilera.jul@gmail.com>
2017-09-16 16:27:25 +0200

    Fix bugs with menus and Design

--- Assets/GameAssets/_Scripts/Menu/UIMenuActions.cs
+++ Assets/GameAssets/_Scripts/Menu/UIMenuActions.cs
@@ -1,10 +1,13 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIMenuActions : MonoBehaviour
 {
-    [SerializeField] private Texture2D _mouseTexture;
+    [SerializeField]
+    private Texture2D _mouseTexture;
 
     void Start()
     {
@@ -13,6 +16,15 @@ public class UIMenuActions : MonoBehaviour
 
     public void PlayGame(string sceneName)
     {
+        GlobalActions.IsShotGunDroped = false;
+        GlobalActions.ChangedWeapon = false;
+        GlobalActions.HasRevolver = false;
+        GlobalActions.HasMachineGun = false;
+        GlobalActions.HasShotGun = false;
+        GlobalActions.IsBossDead = false;
+        GlobalActions.BossDrop = false;
+        GlobalActions.PlayerHasStone = false;
+
         SceneManager.LoadScene(sceneName);
     }
 
