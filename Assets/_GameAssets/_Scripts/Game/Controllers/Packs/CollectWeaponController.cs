99c94b449ea0c4800c679ab906c026e25af7b75d
Julio Aguilera <aguilera.jul@gmail.com>
2018-06-13 02:01:41 +0200

    develop: update gameplay scene elements

---
+++ Assets/_GameAssets/_Scripts/Game/Controllers/Packs/CollectWeaponController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class CollectWeaponController : MonoBehaviour
+{
+    public float radioToCollect = 1f;
+    public GameObject weaponPrefab;
+    public AudioClip collectedWeaponClip;
+    public string nameToDisplayUser;
+
+    private WeaponController[] _weaponsController;
+    private GameObject _player;
+
+    // Use this for initialization
+    void Awake()
+    {
+        this._weaponsController = FindObjectsOfType<WeaponController>();
+        this._player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(_player.transform.position, this.transform.position);
+        if (distance <= radioToCollect)
+        {
+            foreach (var weaponController in _weaponsController)
+            {
+                bool isWeaponActivated = weaponController.ActivateWeaponByName(weaponPrefab.name);
+                if (collectedWeaponClip != null && isWeaponActivated)
+                {
+                    AudioSource.PlayClipAtPoint(collectedWeaponClip, this.transform.position, 1);
+                    this.gameObject.SetActive(false);
+                    break;
+                }
+            }
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(this.transform.position, radioToCollect);
+        Gizmos.color = Color.blue;   
+    }
+}
